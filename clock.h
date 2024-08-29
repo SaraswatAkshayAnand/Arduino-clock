6215f52bae1df211c5d70b15ce8c08196151a420
Saraswat Akshay Anand <154507154+SaraswatAkshayAnand@users.noreply.github.com>
2024-08-29 23:35:45 +0530

    Add files via upload

---
+++ clock.h
@@ -0,0 +1,269 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <Arduino.h>
+#include "datetime.h"
+
+// Basically a big switch where each case is a separate object
+
+class Clock;
+
+class State {
+protected:
+    Clock *clock;
+    int counter;
+
+public:
+    State() : counter(0) {};
+    virtual ~State() {};
+
+    void set_context(Clock *clock) { this->clock = clock; }
+
+    virtual void tick() = 0;
+    virtual void up() = 0;
+    virtual void down() = 0;
+    virtual void date() = 0;
+    virtual void time() = 0;
+};
+
+
+class Clock {
+private:
+    State *state;
+
+public:
+    DateTime date_time;
+    char date_buf[11];
+    char time_buf[9];
+    volatile bool buf_changed;
+
+    Clock(State *st) : state(st) {
+        state->set_context(this);
+        refresh_bufs();
+    }
+
+    ~Clock() {
+        if (state != nullptr) delete state;
+    }
+
+    void change_state(State *st) {
+        if (state != nullptr) delete state;
+        state = st;
+        state->set_context(this);
+
+        refresh_bufs();
+    }
+
+    void tick() { state->tick(); }
+    void up() { state->up(); }
+    void down() { state->down(); }
+    void date() { state->date(); }
+    void time() { state->time(); }
+
+    bool has_buf_changed() {
+        if (!buf_changed) return false;
+
+        buf_changed = false;
+        return true;
+    }
+
+    void refresh_bufs() {
+        snprintf(time_buf, 9, "%02d:%02d:%02d",
+            date_time.hour, date_time.minute, date_time.second);
+
+        snprintf(date_buf, 11, "%02d.%02d.%04d",
+            date_time.day, date_time.month, date_time.year);
+        buf_changed = true;
+    }
+};
+
+
+class Blinking : public State {
+private:
+    bool displayed = true;
+
+public:
+    void blink(int cmp_val, void *buf, size_t n) {
+        if (++counter == cmp_val) {
+            counter = 0;
+            if (displayed) {
+                memset(buf, ' ', n);
+                clock->buf_changed = true;
+            }
+            else clock->refresh_bufs();
+
+            displayed = !displayed;
+        }
+    }
+
+    void datetime_modified() {
+        counter = 0;
+        displayed = true;
+        clock->refresh_bufs();
+    }
+};
+
+
+class ChangeSeconds : public Blinking {
+public:
+    void tick() {
+        blink(200, clock->time_buf + 6, 2);
+    }
+    void up() {
+        clock->date_time.increment_seconds();
+        datetime_modified();
+    }
+    void down() {
+        clock->date_time.decrement_seconds();
+        datetime_modified();
+    }
+    void date() {
+        return;
+    }
+    void time();
+};
+
+
+class ChangeMinutes : public Blinking {
+public:
+    void tick() {
+        blink(200, clock->time_buf + 3, 2);
+    }
+    void up() {
+        clock->date_time.increment_minutes();
+        datetime_modified();
+    }
+    void down() {
+        clock->date_time.decrement_minutes();
+        datetime_modified();
+    }
+    void date() {
+        return;
+    }
+    void time() {
+        clock->change_state(new ChangeSeconds);
+    }
+};
+
+
+class ChangeHours : public Blinking {
+public:
+    void tick() {
+        blink(200, clock->time_buf, 2);
+    }
+    void up() {
+        clock->date_time.increment_hours();
+        datetime_modified();
+    }
+    void down() {
+        clock->date_time.decrement_hours();
+        datetime_modified();
+    }
+    void date() {
+        return;
+    }
+    void time() {
+        clock->change_state(new ChangeMinutes);
+    }
+};
+
+
+class ChangeYear : public Blinking {
+public:
+    void tick() {
+        blink(200, clock->date_buf + 6, 4);
+    }
+    void up() {
+        clock->date_time.increment_year();
+        datetime_modified();
+    }
+    void down() {
+        clock->date_time.decrement_year();
+        datetime_modified();
+    }
+    void date();
+    void time() {
+        return;
+    }
+};
+
+
+class ChangeMonth : public Blinking {
+public:
+    void tick() {
+        blink(200, clock->date_buf + 3, 2);
+    }
+    void up() {
+        clock->date_time.increment_month();
+        datetime_modified();
+    }
+    void down() {
+        clock->date_time.decrement_month();
+        datetime_modified();
+    }
+    void date() {
+        clock->change_state(new ChangeYear);
+    }
+    void time() {
+        return;
+    }
+};
+
+
+class ChangeDay : public Blinking {
+public:
+    void tick() {
+        blink(200, clock->date_buf, 2);
+    }
+    void up() {
+        clock->date_time.increment_day();
+        datetime_modified();
+    }
+    void down() {
+        clock->date_time.decrement_day();
+        datetime_modified();
+    }
+    void date() {
+        clock->change_state(new ChangeMonth);
+    }
+    void time() {
+        return;
+    }
+};
+
+
+class Working : public State {
+public:
+    void tick() {
+        if (++counter == 500) {
+            counter = 0;
+            clock->date_time.tick();
+            clock->refresh_bufs();
+        }
+    };
+    void up() {
+        return;
+    }
+    void down() {
+        return;
+    }
+    void date() {
+        clock->change_state(new ChangeDay);
+    }
+    void time() {
+        clock->change_state(new ChangeHours);
+    }
+};
+
+
+void ChangeSeconds::time() {
+    clock->date_time.adjust_day_after_change();
+    clock->change_state(new Working);
+}
+
+void ChangeYear::date() {
+    clock->date_time.adjust_day_after_change();
+    clock->change_state(new Working);
+}
+
+#endif /* CLOCK_H */
---
+++ datetime.cpp
@@ -0,0 +1,120 @@
+#include <Arduino.h>
+#include "datetime.h"
+
+DateTime::DateTime() : day(28), month(5), year(2024), hour(23), minute(59), second(40) {}
+
+void DateTime::tick() {
+    if (++second < 60) return;
+    second = 0;
+
+    if (++minute < 60) return;
+    minute = 0;
+
+    if (++hour < 24) return;
+    hour = 0;
+
+    day++;
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            if (day <= 31) return;
+            break;
+
+        case 4: case 6: case 9: case 11:
+            if (day <= 30) return;
+            break;
+
+        case 2:
+            if (day <= (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 29 : 28)) {
+                return;
+            }
+            break;
+    }
+
+    day = 1;
+    if (++month <= 12) return;
+
+    month = 1;
+    year++;
+}
+
+void DateTime::increment_day() {
+    if (++day > 31) day = 1;
+}
+
+void DateTime::decrement_day() {
+    if (--day == 0) day = 31;
+}
+
+void DateTime::increment_month() {
+    if (++month > 12) month = 1;
+}
+
+void DateTime::decrement_month() {
+    if (--month == 0) month = 12;
+}
+
+void DateTime::increment_year() {
+    year++;
+}
+
+void DateTime::decrement_year() {
+    year--;
+}
+
+void DateTime::adjust_day_after_change() {
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            if (day > 31) day = 31;
+            break;
+
+        case 4: case 6: case 9: case 11:
+            if (day > 30) day = 30;
+            break;
+
+        case 2:
+            byte days_in_feb = (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 29 : 28);
+            if (day > days_in_feb) {
+                day = days_in_feb;
+            }
+            break;
+    }
+}
+
+void DateTime::increment_seconds() {
+    if (++second >= 60) second = 0;
+}
+
+void DateTime::decrement_seconds() {
+    if (second > 0) {
+        second--;
+    }
+    else {
+        second = 59;
+    }
+}
+
+void DateTime::increment_minutes() {
+    if (++minute >= 60) minute = 0;
+}
+
+void DateTime::decrement_minutes() {
+    if (minute > 0) {
+        minute--;
+    }
+    else {
+        minute = 59;
+    }
+}
+
+void DateTime::increment_hours() {
+    if (++hour >= 24) hour = 0;
+}
+
+void DateTime::decrement_hours() {
+    if (hour > 0) {
+        hour--;
+    }
+    else {
+        hour = 23;
+    }
+}
